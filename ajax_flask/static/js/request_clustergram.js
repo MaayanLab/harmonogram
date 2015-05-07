fdea2754b091bf3f7ebfc9635397f3c1a478536f
cornhundred <nickfloresfernandez@gmail.com>
2015-05-07 14:04:52 -0400

    improving file and function names

--- ajax_flask/static/js/request_clustergram.js
+++ ajax_flask/static/js/request_clustergram.js
@@ -50,3 +50,26 @@ $( "#searchForm" ).submit( function( event ) {
 
 
 });
+
+
+function make_new_map(network_data){
+  // remove old map
+  d3.select("#main_svg").remove();
+  d3.select('#kinase_substrates').remove();
+
+  // wait message 
+  d3.select('.blockMsg').select('h1').text('Waiting for matrix to load...');
+
+  // pass in data and callback function 
+  start_making_exp_map(network_data, make_svg_enrichment_genes );
+
+};
+
+// get parameters from sim_matrix before the matrix is made
+function start_making_exp_map(sim_matrix, callback) {
+
+  // after all of the variables have been initialized then run the callback function 
+  callback(sim_matrix); 
+};
+
+
