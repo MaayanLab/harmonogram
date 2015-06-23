f605e8a9f0f02adcc37326f681a53d1718a23206
cornhundred <nickfloresfernandez@gmail.com>
2015-06-23 16:10:33 -0400

    working buttons, need to add colors to resource types and add some sort of key for the resource-class colors, then hover effects on row and column labels

--- harminogram/static/js/load_local_network.js
+++ harminogram/static/js/load_local_network.js
@@ -1,27 +1,35 @@
+function load_class_clustergram(inst_prot_class){
 
-// set up wait message before request is made 
-$.blockUI({ css: { 
-        border: 'none', 
-        padding: '15px', 
-        backgroundColor: '#000', 
-        '-webkit-border-radius': '10px', 
-        '-moz-border-radius': '10px', 
-        opacity: .8, 
-        color: '#fff' 
-    } });
+	// only make clustergram if its a new class
+	if (inst_prot_class != glob_prot_class)	{
 
+		// save global variable
+		glob_prot_class = inst_prot_class;
 
+		// set up wait message before request is made 
+		$.blockUI({ css: { 
+		        border: 'none', 
+		        padding: '15px', 
+		        backgroundColor: '#000', 
+		        '-webkit-border-radius': '10px', 
+		        '-moz-border-radius': '10px', 
+		        opacity: .8, 
+		        color: '#fff' 
+		    } });
 
-// use d3 to load a json 
-d3.json('/harminogram/static/networks/KIN_cumul_probs.json', function(network_data){
+		// use d3 to load a json 
+		d3.json('/harminogram/static/networks/'+inst_prot_class+'_cumul_probs.json', function(network_data){
 
-	// make global copy of network_data 
-	global_network_data = network_data;
+			// make global copy of network_data 
+			global_network_data = network_data;
 
-	// pass the network data to d3_clustergram 
-	make_d3_clustergram(network_data);
+			// pass the network data to d3_clustergram 
+			make_d3_clustergram(network_data);
 
-  // turn off the wait sign 
-  $.unblockUI();
+		  // turn off the wait sign 
+		  $.unblockUI();
 
-});
+		});
+
+	};
+};
