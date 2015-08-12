453f3c6601a1cb6be73d5c79c9ed21b59bc1a0c8
cornhundred <doctorcorn2002@gmail.com>
2015-08-12 12:46:50 -0400

    trying to get button active state to work

--- harmonogram/static/js/load_local_network.js
+++ harmonogram/static/js/load_local_network.js
@@ -9,6 +9,16 @@ function load_class_clustergram(inst_prot_class){
 		// save global variable
 		glob_prot_class = inst_prot_class;
 
+		// change active button states
+		d3.select('#class_buttons')
+			.selectAll('label')
+			.attr('class','btn btn-primary prot_class');
+
+		console.log(this)
+		// d3.select(this)
+		// 	.attr('class','btn btn-primary prot_class active');
+
+
 		// set up wait message before request is made 
 		$.blockUI({ css: { 
 		        border: 'none', 
@@ -23,11 +33,45 @@ function load_class_clustergram(inst_prot_class){
 		// use d3 to load a json 
 		d3.json('/harmonogram/static/networks/'+inst_prot_class+'_cumul_probs.json', function(network_data){
 
-			// make global copy of network_data 
-			global_network_data = network_data;
+			// // make global copy of network_data 
+			// global_network_data = network_data;
+			// // pass the network data to d3_clustergram 
+			// make_d3_clustergram(network_data);
 
-			// pass the network data to d3_clustergram 
-			make_d3_clustergram(network_data);
+	    // define the outer margins of the visualization 
+	    var outer_margins = {
+	        'top':1,
+	        'bottom':1,
+	        'left':225,
+	        'right':1
+	      };
+
+	    // define callback function for clicking on tile 
+	    function click_tile_callback(tile_info){
+	      console.log('my callback')
+	      console.log('clicking on ' + tile_info.row + ' row and ' + tile_info.col + ' col with value ' + String(tile_info.value))
+	    };
+
+	    // define arguments object 
+	    var arguments_obj = {
+	      'network_data': network_data,
+	      'svg_div_id': 'svg_div',
+	      'row_label':'Genes',
+	      'col_label':'Resources',
+	      'outer_margins': outer_margins,
+	      // 'opacity_scale':'log',
+	      // 'input_domain':7,
+	      'tile_colors':['#000000','#1C86EE'],
+	      'title_tile': true,
+	      // 'click_tile': click_tile_callback,
+	      // 'click_group': click_group_callback
+	      // 'resize':false
+	      // 'order':'rank'
+	    };
+
+	    // make clustergram: pass network_data and the div name where the svg should be made 
+	    d3_clustergram.make_clust( arguments_obj );
+
 
 		  // turn off the wait sign 
 		  $.unblockUI();
@@ -151,40 +195,4 @@ function find_gene_in_clust(){
 
 };
 
-// // initialize typeahead 
-// function initialize_typeahead(){
-// 	// implementing typeahead
-// 	////////////////////////////
-// 	var substringMatcher = function(strs) {
-// 		// strs are the list of possible strings 
-// 	  return function findMatches(q, cb) {
-// 	    var matches, substringRegex;
-// 	    // an array that will be populated with substring matches
-// 	    matches = [];
-// 	    // regex used to determine if a string contains the substring `q`
-// 	    substrRegex = new RegExp(q, 'i');
-// 	    // iterate through the pool of strings and for any string that
-// 	    // contains the substring `q`, add it to the `matches` array
-// 	    $.each(strs, function(i, str) {
-// 	      if (substrRegex.test(str)) {
-// 	        matches.push(str);
-// 	      }
-// 	    });
-// 	    cb(matches);
-// 	  };
-// 	};
-// 	// // first remove any old data in typeahead
-// 	// $('#gene_search_box').typeahead('destroy');
-// 	// select the container and the input box 
-// 	$('#gene_search_box')
-// 		.typeahead(
-// 			{
-// 			  hint: true,
-// 			  highlight: true,
-// 			  minLength: 1
-// 			},
-// 			{
-// 			  source: substringMatcher(all_genes)
-// 			});
-// };	
 
