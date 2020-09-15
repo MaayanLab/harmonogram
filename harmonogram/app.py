832b09415241975c16b3cf43990204566361a096
Daniel Clarke <u8sand@gmail.com>
2020-09-15 16:45:00 -0400

    Updated from actual docker container

--- harmonogram/app.py
+++ harmonogram/app.py
@@ -9,8 +9,8 @@ from logging.handlers import RotatingFileHandler
 import os
 from flask import send_from_directory
 
-# # change routing of logs when running docker 
-# logging.basicConfig(stream=sys.stderr) 
+# # change routing of logs when running docker
+# logging.basicConfig(stream=sys.stderr)
 
 
 
@@ -19,26 +19,26 @@ app = Flask(__name__, static_url_path='')
 
 ENTRY_POINT = '/harmonogram'
 
-# switch for local and docker development 
+# switch for local and docker development
 # docker_vs_local
 ##########################################
 
-# for local development /
-SERVER_ROOT = os.path.dirname(os.getcwd()) + '/harmonogram/harmonogram' 
+# # for local development /
+# SERVER_ROOT = os.path.dirname(os.getcwd()) + '/harmonogram/harmonogram'
 
-# # for docker development
-# SERVER_ROOT = '/app/harmonogram'
+# for docker development
+SERVER_ROOT = '/app/harmonogram'
 
 #######################################
 
-@app.route(ENTRY_POINT + '/<path:path>') ## original 
+@app.route(ENTRY_POINT + '/<path:path>') ## original
 # @crossdomain(origin='*')
 def send_static(path):
 
   # print('path and SERVER_ROOT')
   # print(path)
   # print(SERVER_ROOT)
-  
+
   return send_from_directory(SERVER_ROOT, path)
 
 
@@ -50,37 +50,36 @@ def index():
 # post request
 @app.route('/harmonogram/', methods=['GET','POST'])
 def python_function():
-  import flask 
+  import flask
   import make_enr_clust
-  import json 
-  import json_scripts 
+  import json
+  import json_scripts
   import sys
   import cookielib, poster, urllib2, json
 
-  error = None 
+  error = None
 
   # get gmt_colors
   gmt_colors = json.loads(request.form['gmt_colors'])
 
-  # get the number of enriched terms 
+  # get the number of enriched terms
   num_terms = int(request.form['num_terms'])
 
-  # get the genes from the request 
+  # get the genes from the request
   inst_genes = request.form['genes'].strip().split('\n')
 
-  # convert to uppercase 
+  # convert to uppercase
   inst_genes = [x.upper().strip() for x in inst_genes]
 
-  # obtain unique genes 
+  # obtain unique genes
   inst_genes = list(set(inst_genes))
 
-  # calc enrichment and cluster 
+  # calc enrichment and cluster
   network = make_enr_clust.main(gmt_colors, inst_genes, num_terms, 'jaccard')
 
-  # jsonify a list of dicts 
+  # jsonify a list of dicts
   return flask.jsonify( network )
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000,debug=True)
- 
